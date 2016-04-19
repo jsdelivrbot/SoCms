b713d725edf8334745b38c86e558605b792026b8
WheeleBin <wheelebin@users.noreply.github.com>
2016-04-19 12:52:41 +0200

    Update no.js

--- js/no.js
+++ js/no.js
@@ -1,1 +1,13 @@
+$.getScript("https://cdn.rawgit.com/sjovanovic/xpull/master/xpull.js", function(){
+
+   alert("Script loaded but not necessarily executed.");
+
+});
+
 console.log("This that new shit", $('.main'));
+
+$('.day-wrap').xpull({
+    'pullThreshold':50,
+    'callback':function(){ alert(111); },
+    'spinnerTimeout':2000
+ });
