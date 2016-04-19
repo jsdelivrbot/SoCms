70702e002f654d9442898d385074d3b2c509ab20
WheeleBin <wheelebin@users.noreply.github.com>
2016-04-19 12:56:45 +0200

    s

--- js/no.js
+++ js/no.js
@@ -6,7 +6,7 @@ $.getScript("https://cdn.rawgit.com/sjovanovic/xpull/master/xpull.js", function(
 
 console.log("This that new shit", $('.main'));
 
-$('.day-wrap').xpull({
+$('.main').xpull({
     'pullThreshold':50,
     'callback':function(){ alert(111); },
     'spinnerTimeout':2000
